f8b167e1195553c9a21acb058309cc8312ce2a99
YanLieven <yan_brasil10@hotmail.com>
2014-12-17 00:45:07 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,63 +3,72 @@
 
 void help()
 {
-    qDebug() << "Para que o programa execute a compressão, é necessário digitar:";
-    qDebug() << "1º - Nome do arquivo a ser Comprimido/Descomprimido, 2º - Nome do arquivo de saída.";
-    qDebug() << "//--------------------------------------------------------------------";
+    qDebug() << "//------------------------------------------------------------------------------";
     qDebug() << "Se for comprimir um arquivo, esse não deve possuir formato '.huff',";
+    qDebug() << "é necessário digitar 1º- '-c', 2º- 'nome do arquivo original'.";
     qDebug() << "O arquivo de saída terá formato .huff automaticamente.";
-    qDebug() << "//--------------------------------------------------------------------";
+    qDebug() << "//------------------------------------------------------------------------------";
     qDebug() << "Se for descomprimir um arquivo, esse deve possuir obrigatoriamente formato '.huff',";
-    qDebug() << "O arquivo de saída pode ter o formato mais desejado.";
-    qDebug() << "//--------------------------------------------------------------------";
-    qDebug() << "SOMENTE SERÃO ACEITOS 2(DOIS) ARGUMENTOS.";
+    qDebug() << "//------------------------------------------------------------------------------";
+    qDebug() << "SOMENTE SERÃO ACEITOS 2(DOIS) ARGUMENTOS NA COMPRESSÃO E 1(UM) NA DESCOMPRESSÃO.";
+    qDebug() << "//------------------------------------------------------------------------------";
+    qDebug() << "para mostrar essa mensagem novamente, execute o programa com 1 argumento apenas: '-h'.";
+    qDebug() << "//------------------------------------------------------------------------------;";
 }
 
 int main(int argc, char *argv[])
 {
+    Huffman huff;
     QCoreApplication a(argc, argv);
-    if(argc == 2) // O programa só aceita 1 ou dois argumentos como entrada.
+    if(argc == 3)
     {
-        if(strcmp(argv[1],"help") == 0)
-            help();
-        else
-            qDebug() << argv[1] << "<-- este comando não é reconhecido pelo programa.";
-        exit(1);
-    }
-    else if(argc == 3)
-    {
-        Huffman huff; // Uma variável da nossa classe Huffman que é essencial no programa
-        QString file = argv[1], newfile = argv[2];
-        file = file.remove(0, file.size() - 5);
-        newfile = newfile.remove(0, newfile.size() - 5);
-        if(strcmp(file, ".huff") == 0 && strcmp(newfile, ".huff") != 0)
+        QString com(argv[1]), file(argv[2]), filef;
+        filef = file.remove(0, file.size() - 4);
+        if(!com.compare("-c"))
         {
-            file = argv[1];
-            newfile = argv[2];
-            huff.descomprimir(file, newfile);
-        }
-        else if(strcmp(file, ".huff") != 0)
-        {
-            if(strcmp(newfile, ".huff") == 0)
-                newfile = argv[2];
+            if(filef.compare("huff"))
+            {
+                file = argv[2];
+                filef = file.remove(file.size() - 4, 4);
+                file = argv[2];
+                filef.append(".huff");
+                huff.comprimir(file, filef);
+            }
             else
             {
-                newfile = argv[2];
-                newfile = newfile.remove(newfile.size() - 4, 4).append(".huff");
+                qDebug() << "O arquivo de entrada não pode ter formato '.huff'.";
+                help();
+                exit(1);
             }
-            file = argv[1];
-            huff.comprimir(file, newfile);
         }
         else
         {
-            qDebug() << "Os dois argumentos são do tipo .huff, não há operação possível no programa";
+            qDebug() << "O primeiro argumento não é reconhecido pelo programa.";
+            help();
+            exit(1);
+        }
+    }
+    else if(argc == 2)
+    {
+        QString file(argv[1]), filef;
+        filef = file.remove(0, file.length() - 4);
+        file = argv[1];
+        if(!file.compare("-h"))
+            help();
+        else if(!filef.compare("huff"))
+            huff.descomprimir(file);
+        else
+        {
+            qDebug() << "O arquivo não possui formato '.huff'!.";
+            help();
             exit(1);
         }
     }
     else
     {
-        qDebug() << "O número de argumentos passados não está correto.";
+        qDebug() << "Não foi passado um número correto de argumentos para o programa.";
+        help();
         exit(1);
     }
-    return a.exec();
+    return 0;
 }
